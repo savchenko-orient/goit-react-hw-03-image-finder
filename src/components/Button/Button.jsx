4fbb9beb30e06c4222bb66d6703bdb162dd0a053
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-03-27 19:50:17 +0300

    fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,6 +4,7 @@ import css from './Button.module.css';
 const Button = ({ buttonText, handleLoadMore }) => {
   return (
     <button onClick={handleLoadMore} className={css.Button}>
+
       {buttonText}
     </button>
   );
